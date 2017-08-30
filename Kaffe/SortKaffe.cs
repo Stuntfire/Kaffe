f7af94efeb4b122d320457f3b6808c389abd584b
Søren Roed Schack <soer622t@edu.easj.dk>
2017-08-30 09:51:50 +0200

    Tilføjet SortKaffe klassen med test.

--- Kaffe/SortKaffe.cs
+++ Kaffe/SortKaffe.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Kaffe
 {
-    public class Class1
+    public class SortKaffe : Kaffe
     {
+        public override int pris()
+        {
+            return 20;
+        }
     }
 }
---
+++ KaffeTests/KaffeTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kaffe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffe.Tests
+{
+    [TestClass()]
+    public class KaffeTests
+    {
+        [TestMethod()]
+        public void PrisTestKaffe()
+        {
+            //Arrange
+            var Kaffe = new SortKaffe();
+
+            //Act
+            int pris = Kaffe.pris();
+
+            //Assert
+            Assert.AreEqual(20, pris);
+        }
+
+        [TestMethod()]
+        public void StyrkeTestKaffe()
+        {
+            //Arrange
+            var Kaffe = new SortKaffe();
+
+            //Act
+            string styrke = Kaffe.styrke();
+
+            //Assert
+            Assert.AreEqual("stærk", styrke);
+        }
+    }
+}
